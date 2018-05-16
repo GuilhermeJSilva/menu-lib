4251e8eb8625f92d1cb9de0cdbd4f917e6d4aa83
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-05-16 10:46:28 +0100

    Read string with spaces

--- menu-lib/ReadFunctions.hpp
+++ menu-lib/ReadFunctions.hpp
@@ -24,3 +24,24 @@ T read_value(std::string const &prompt_message, std::string const &error_message
     }
     return read_value;
 }
+
+std::string
+read_string(std::string const &prompt_message, std::string const &error_message = "Invalid choice try again.",
+            bool (*f)(std::string) = [](std::string i) { return true; }) {
+    std::string read_value;
+    std::cout << prompt_message << std::endl;
+    getline(std::cin, read_value);
+
+    while (std::cin.fail() || !f(read_value)) {
+        std::cout << error_message << std::endl;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+        std::cout << prompt_message << std::endl;
+        getline(std::cin, read_value);
+    }
+    return read_value;
+}
